83b665a804e4e8ab58874fd944bd0bb99038dc5d
kblow-user <kblow.sg@outlook.com>
2025-10-05 13:26:22 +0800

    first commit

---
+++ app/dashboard/page.js
@@ -0,0 +1,8 @@
+export default async function Dashboard() {
+  return (
+    <main>
+      <h1>Collect customer feedback</h1>
+      <div>Create a feedback board in a minutes</div>
+    </main>
+  );
+}
