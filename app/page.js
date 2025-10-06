ec3a3e99a366c4405cee5388447f6a31053cc0bd
kblow-user <kblow.sg@outlook.com>
2025-10-06 13:56:28 +0800

    commit on app page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,110 @@
-import Image from "next/image";
+import ButtonLogin from "@/components/ButtonLogin";
 
 export default function Home() {
+  const isLoggedIn = true;
+  const name = "Marc";
+
   return (
     <main>
-      <button className="btn">Get started</button>
+      <section className="bg-fuchsia-200">
+        <div className="max-w-3xl mx-auto flex justify-between items-center">
+          <div className="font-bold">CodeFastSaaS</div>
+          <div className="space-x-4 md:bg-red-300">
+            <a className="link link-hover">Pricing</a>
+            <a className="link link-accent">FAQ</a>
+          </div>
+          <div className="bg-green-300">
+            <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+          </div>
+        </div>
+      </section>
+      <section className="text-center py-32 px-8 mb-4">
+        <button className="text-3xl font-bold px-8">Get started</button>
+        {/* <div onClick={() => console.log("Clicked!")}></div> */}
+        <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+      </section>
+      <section className="bg-base-300">
+        <div className="py-32 px-8 max-w-3xl mx-auto">
+          <p className="text-lg uppercase text-center text-primary mb-4">
+            Pricing
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-extralight mb-8 text-center">
+            A pricing that adapts to your needs
+          </h2>
+
+          <div className="p-8 bg-base-100 w-96 rounded-3xl mx-auto">
+            <div className="flex gap-1 items-baseline">
+              <div className="text-4xl font-black">$19</div>
+              <div className="uppercase text-sm font-medium opacity-60">
+                /month
+              </div>
+            </div>
+            <ul className="space-y-2">
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="text-yellow-600 size-4"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Collect customer feedback
+              </li>
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="text-yellow-600 size-4"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Unlimited boards
+              </li>
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="text-yellow-600 size-4"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Admin Support
+              </li>
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="text-yellow-600 size-4"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                24/7 support
+              </li>
+            </ul>
+            <ButtonLogin isLoggedIn={isLoggedIn} extraStyle="w-full" />
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
---
+++ components/ButtonLogin.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const ButtonLogin = ({ isLoggedIn, name, extraStyle }) => {
+  if (isLoggedIn) {
+    return (
+      <Link
+        href="/dashboard"
+        className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
+      >
+        Welcome back {name}
+      </Link>
+    );
+  } else {
+    return <button>Login</button>;
+  }
+};
+
+export default ButtonLogin;
