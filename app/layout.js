88463b2ddfbc8684aaf23689e36d0058f3d079fa
kblow-user <kblow.sg@outlook.com>
2025-10-09 13:22:57 +0800

    api board updated

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="cupcake" className="scroll-smooth">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <div>
+          <Toaster />
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
